664158be4a1cc139dcac048421673b6b7e3ddfd7
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-03-11 20:40:41 -0300

    atts

--- wwwroot/js/modal2.js
+++ wwwroot/js/modal2.js
@@ -4,9 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const parallaxContainer = document.getElementById("parallaxView");
     const parallaxImage = document.getElementById("parallaxImage");
     const closeParallax = document.getElementById("closeParallax");
-    const body = document.body;
 
-    // Abrir o modal ao clicar no botão "Ver Projeto"
+    // Exibir Parallax ao clicar no botão "Ver Projeto"
     viewButtons.forEach((button) => {
         button.addEventListener("click", function (event) {
             event.stopPropagation();
@@ -14,22 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
             const imageSrc = imageContainer.querySelector("img").src;
             parallaxImage.src = imageSrc;
             parallaxContainer.classList.add("visible");
-            body.style.overflow = "hidden"; // Impede rolagem no fundo
         });
     });
 
-    // Fechar o modal ao clicar no botão "✖"
+    // Fechar Parallax ao clicar no botão "✖"
     closeParallax.addEventListener("click", function () {
         parallaxContainer.classList.remove("visible");
-        body.style.overflow = "auto"; // Restaura rolagem
     });
 
     // Fechar ao clicar fora da imagem
     parallaxContainer.addEventListener("click", function (event) {
         if (event.target === parallaxContainer) {
             parallaxContainer.classList.remove("visible");
-            body.style.overflow = "auto"; // Restaura rolagem
         }
     });
 });
-
